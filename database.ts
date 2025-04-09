d33a8a62a5b61bf573384c7cbce915ba7d1dfa2e
Romain🔥 <romaintibo6@gmail.com>
2025-04-09 19:21:14 +0200

    feat: read tables from public schema

--- database.ts
+++ database.ts
@@ -8,3 +8,11 @@ export const db = new SQL({
 	connectionTimeout: 30,
 });
 
+export const getTables = async () =>
+	(await db`
+  SELECT table_name
+  FROM information_schema.tables
+  WHERE table_schema = 'public'
+  AND table_type = 'BASE TABLE';
+`) as Array<{ table_name: string }>;
+
