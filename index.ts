20b3d8be2227b342bea2e1240426009861ec7429
Romain🔥 <romaintibo6@gmail.com>
2025-04-09 20:28:49 +0200

    feat: create folders based on tables name

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import { getTables } from "./database";
+import { copyFolder, createFolder } from "./src/utils";
+
+await copyFolder("./elysia_template", "./src/elysia_template");
+
+const tables = await getTables();
+
+for (const table of tables) {
+	const tableName = table.table_name;
+	createFolder(tableName.toLowerCase(), "./src/elysia_template/src");
+	console.log(`Copied template for ${tableName}`);
+}
